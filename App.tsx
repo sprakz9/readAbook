54e870abf92593d5c72026e0594c4fdc0aa37652
sprakz9 <nonntp9@gmail.com>
2024-06-24 22:24:48 +0700

    Add bottom bar

--- App.tsx
+++ App.tsx
@@ -2,14 +2,43 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
+// import { AntDesign, Entypo, Feather, FontAwesome, FontAwesome5,  Fontisto, Foundation, MaterialCommunityIcons, MaterialIcons 
+// } from '@expo/vector-icons';
+// import { Icon } from 'react-native-paper';
+// import Icon from 'react-native-vector-icons/FontAwesome';
+
 // import Login from './screen/Login';
-import Home from './screen/Home';
-import Mybook from './screen/Mybook';
+import HomePage from './screen/Home';
+import MybookPage from './screen/Mybook';
+
 
 
 const Tab = createMaterialBottomTabNavigator()
-const Stack = createNativeStackNavigator();
+function HomeTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name='หนังสือของฉัน' component={MybookPage}
+      options={{
+        tabBarIcon : () => (
+          <Icon name = "library-outline" color={"green"} size={24} />
+          // <FontAwesome5 name="star" size={24} color="green" />
+        )
+      }}
+      />
+      <Tab.Screen name='ร้านค้า' component={HomePage}
+      options={{
+        tabBarIcon : () => (
+          <Icon name = "storefront-outline" color={"green"} size={24} />
+        )
+      }}
+      />
+  </Tab.Navigator>
+  )
+}
 
+
+const Stack = createNativeStackNavigator();
 const App = () => {
   return (
 
@@ -20,12 +49,9 @@ const App = () => {
           component={Login}
         /> */}
         <Stack.Screen 
-          name="Home" 
-          component={Home} 
-        />
-        <Tab.Screen
-        name = "Mybook"
-        component = {Mybook}
+          name="HomeTabs" 
+          component={HomeTabs} 
+          options={{ headerShown: false }} 
         />
       </Stack.Navigator>
     </NavigationContainer>
