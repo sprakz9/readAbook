a523167c97aca747d048c85662326647bf28b695
sprakz9 <nonntp9@gmail.com>
2024-06-28 07:33:34 +0700

    Update FavBook.tsx

--- screen/FavBook.tsx
+++ screen/FavBook.tsx
@@ -1,5 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, FlatList, Image, TouchableOpacity, ActivityIndicator , RefreshControl } from 'react-native';
+import { 
+  View, 
+  Text, 
+  StyleSheet,
+  FlatList, 
+  Image, 
+  TouchableOpacity, 
+  ActivityIndicator , 
+  RefreshControl, 
+  ScrollView 
+} from 'react-native';
 import firestore from '@react-native-firebase/firestore';
 import { firebase } from '@react-native-firebase/auth';
 import { useNavigation } from '@react-navigation/native';
